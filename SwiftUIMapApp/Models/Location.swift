7dbbd748e885e9c0db7c823cbf623996a42f4786
Chechobuitrago <buitrago12.09@gmail.com>
2024-07-26 15:38:04 -0500

    DataServices, Models, AssetsFolder

---
+++ SwiftUIMapApp/Models/Location.swift
@@ -0,0 +1,21 @@
+//
+//  Location.swift
+//  SwiftUIMapApp
+//
+//  Created by Sergio Buitrago on 26/07/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
+
